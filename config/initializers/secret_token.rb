2df1d1e79ad206dc6d1ee4fcbd8826e1c938e1b4
Shane da Silva <shane@dasilva.io>
2012-12-25 18:52:35 -0800

    Initial commit. These are files created by Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pingpong::Application.config.secret_token = '52e2cbde1f21ea2fdec4da82ea6ee1c161ada4bc45aec0193b868b756166a0a9e219bd9d6d8d615cbe57a9cd73a705d1cb3d2397e2a7dcb6c07de78dad0e5ef6'
