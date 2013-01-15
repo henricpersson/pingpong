93814a59129ac243f4dc9548fbd63bda7ecf25ff
henric.trotzig@gmail.com <henric.trotzig@gmail.com>
2013-01-14 23:29:53 -0800

    Fetch programs from k5play.se

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
+require 'open-uri'
+
 class PagesController < ApplicationController
+
   def index
+    @programs = JSON.parse(
+      open('http://www.kanal5play.se/api/getMobileFindProgramsContent').read)
+    @programs = @programs['programsWithTemperatures']
+
+    @programs.sort! { |a, b| b['temperature'] <=> a['temperature'] }
   end
 end
